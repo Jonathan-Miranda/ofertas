6665918ea2d69b6ce58cc8a6b85d4a3b90280f1a
Jon laptop <jon@laptop.com>
2025-02-18 11:28:40 -0600

    modularizamos el menu (cerrar sesion)

---
+++ src/component/nav.php
@@ -0,0 +1,36 @@
+<nav class="navbar sticky-top navbar-expand-md bg-body-tertiary">
+        <div class="container">
+
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <div class="container">
+                    <div class="row">
+
+                        <div class="col-md-12">
+                            <div class="d-flex justify-content-end">
+
+                                <div class="dropdown">
+                                    <button class="btn btn-secondary dropdown-toggle" type="button"
+                                        data-bs-toggle="dropdown" data-bs-display="static" aria-expanded="false">
+                                        <i class="bi bi-person-circle"></i> Perfil
+                                    </button>
+                                    <ul class="dropdown-menu">
+                                        <li>
+                                            <a class="dropdown-item link-danger" href="src/destroy.php"><i
+                                                    class="bi bi-x-circle"></i> Cerrar sesión</a>
+                                        </li>
+                                    </ul>
+                                </div>
+
+                            </div>
+                        </div>
+
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </nav>
