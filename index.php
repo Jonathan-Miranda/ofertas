d96443052d110b60f0ecaf0c0a7692aea75a79d6
Jon laptop <jon@laptop.com>
2025-02-18 11:15:31 -0600

    modularizamos los archivos css y js en el login

--- index.php
+++ index.php
@@ -5,10 +5,9 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Login</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <?php
+    require('src/component/fonts-bootstrap.php');
+    ?>
     <link rel="stylesheet" href="css/style.css">
 </head>
 
@@ -23,7 +22,7 @@
 
                 <div class="card shadow-sm">
                     <div class="card-body p-5">
-                        <form id="registro" method="POST" name="registro" enctype="multipart/form-data" accept-charset="utf-8">
+                        <form id="frm-login" method="POST" name="registro" enctype="multipart/form-data" accept-charset="utf-8">
 
                             <div class="form-floating mb-3">
                                 <input type="email" class="form-control" id="email" name="email"
@@ -47,7 +46,10 @@
             </div>
         </div>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <?php
+    require('js/jquery-boot-sweetalert.php');
+    require('js/login.php');
+    ?>
 </body>
 
 </html>
