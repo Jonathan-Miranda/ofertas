6ef843bc848d0cfeeb766bb799c95a5b59950244
Jon laptop <jon@laptop.com>
2025-02-21 13:44:13 -0600

    agregamos animacion de confetti cuando se canjea el producto gratis

--- js/slide-add-rm-user.php
+++ js/slide-add-rm-user.php
@@ -232,19 +232,52 @@
             processData: false,  // Evita que jQuery intente procesar los datos
             contentType: false,
             success: function (response) {
-                if (response.icon == "success") {
-                    Swal.fire({
-                        icon: response.icon,
-                        title: response.msj,
-                    }).then(() => {
-                        window.location.href = "user.php";
-                    });
-                } else {
-                    Swal.fire({
-                        icon: response.icon,
-                        title: response.msj,
-                    });
+                Swal.fire({
+                    icon: response.icon,
+                    title: response.msj,
+                }).then(() => {
+                    window.location.href = "user.php";
+                });
+
+                const count = 400,
+                    defaults = {
+                        origin: { y: 0.7 },
+                    };
+
+                function fire(particleRatio, opts) {
+                    confetti(
+                        Object.assign({}, defaults, opts, {
+                            particleCount: Math.floor(count * particleRatio),
+                        })
+                    );
                 }
+
+                fire(0.25, {
+                    spread: 26,
+                    startVelocity: 55,
+                });
+
+                fire(0.2, {
+                    spread: 60,
+                });
+
+                fire(0.35, {
+                    spread: 100,
+                    decay: 0.91,
+                    scalar: 0.8,
+                });
+
+                fire(0.1, {
+                    spread: 120,
+                    startVelocity: 25,
+                    decay: 0.92,
+                    scalar: 1.2,
+                });
+
+                fire(0.1, {
+                    spread: 120,
+                    startVelocity: 45,
+                });
             },
             error: function (xhr, status, error) {
                 Swal.fire({
