d429891c6ea4afde4872581bf0456cfd43a7f369
Jon laptop <jon@laptop.com>
2025-02-24 16:00:47 -0600

    vaceamos de forma dinamica la data de venta de cada lab en la grafica

---
+++ admin/js/dashboard.php
@@ -0,0 +1,90 @@
+<script>
+    const lab = document.getElementById('lab');
+    const venta = document.getElementById('vendidos');
+    const canje = document.getElementById('canje');
+
+    fetch('src/components/data-venta.php')
+        .then(response => response.json())
+        .then(data => {
+            const labels = data.map(item => item.NOMBRE);
+            const ventas = data.map(item => item.total_ventas);
+
+            new Chart(lab, {
+                type: 'bar',
+                data: {
+                    labels: labels,
+                    datasets: [{
+                        label: 'Ventas',
+                        data: ventas,
+                        backgroundColor: 'rgba(54, 162, 235, 0.5)',
+                        borderColor: 'rgba(54, 162, 235, 1)',
+                        borderWidth: 1,
+                        borderRadius: 10
+                    }]
+                },
+                options: {
+                    plugins: {
+                        legend: {
+                            display: false
+                        }
+                    },
+                    scales: {
+                        y: {
+                            beginAtZero: true
+                        }
+                    }
+                }
+            });
+        })
+        .catch(error => console.error('Error obteniendo los datos:', error));
+
+
+
+
+    new Chart(venta, {
+        type: 'bar',
+        data: {
+            labels: ['Ky6', 'Fayrus', 'Portem', 'Tribedoce', 'Lo-bruquin', 'Afleno', 'Tarmin', 'Brunadol'],
+            datasets: [{
+                label: 'Ventas',
+                data: [12, 19, 3, 5, 2, 3, 8, 5],
+                borderWidth: 1,
+                borderRadius: 20
+            }]
+        },
+        options: {
+            plugins: {
+                legend: {
+                    display: false
+                }
+            },
+            indexAxis: 'y',
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+
+
+    new Chart(canje, {
+        type: 'doughnut',
+        data: {
+            labels: ['Ky6', 'Fayrus', 'Portem', 'Tribedoce', 'Lo-bruquin', 'Afleno', 'Tarmin', 'Brunadol'],
+            datasets: [{
+                label: 'Canje',
+                data: [12, 19, 3, 5, 2, 3, 8, 5],
+                borderWidth: 1,
+                borderRadius: 20
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+</script>
