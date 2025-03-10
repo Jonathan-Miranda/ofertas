9c25a719d146ff317098be2a34388f2d73f91a14
Jon laptop <jon@laptop.com>
2025-03-10 16:23:38 -0600

    agregamos validacion para la oferta dinamica y el insert en la db

--- admin/src/add-prod.php
+++ admin/src/add-prod.php
@@ -8,8 +8,11 @@ if (isset($_SESSION['ad-name']) && $_SESSION["rol"] === 0) {
     $name = (isset($_POST['nombre'])) ? $_POST['nombre'] : '';
     $lab = (isset($_POST['lab'])) ? $_POST['lab'] : '';
 
+    $compra = (isset($_POST['compra'])) ? $_POST['compra'] : '';
+    $oferta = (isset($_POST['oferta'])) ? $_POST['oferta'] : '';
+
     //Funciones{
-    function addProd($name, $lab, $con)
+    function addProd($name, $lab, $compra, $oferta, $con)
     {
         // Validar datos de entrada
         if (empty($name) || empty($lab)) {
@@ -19,11 +22,19 @@ if (isset($_SESSION['ad-name']) && $_SESSION["rol"] === 0) {
             ];
         }
 
+        if ($compra < $oferta) {
+            return [
+                'icon' => 'error',
+                'msj' => 'Oferta incorrecta, revisa la estructura (la compra no puede ser menor a lo que se paga)'
+            ];
+        }
+
         try {
-            $q = "INSERT INTO product (NOMBRE, ID_LAB) VALUES (:name, :id)";
+            $q = "INSERT INTO product (NOMBRE, ID_LAB, OFERTA) VALUES (:name, :id, :oferta)";
             $res = $con->prepare($q);
             $res->bindParam(':name', $name, PDO::PARAM_STR);
             $res->bindParam(':id', $lab, PDO::PARAM_INT);
+            $res->bindParam(':oferta', $oferta, PDO::PARAM_INT);
 
             if ($res->execute()) {
                 $icon = "success";
@@ -50,7 +61,7 @@ if (isset($_SESSION['ad-name']) && $_SESSION["rol"] === 0) {
 
     //funcion principal
 
-    $response = addProd($name, $lab, $con);
+    $response = addProd($name, $lab, $compra, $oferta, $con);
 
     //end funcion pricipal
     print json_encode($response);
