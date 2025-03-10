867264bce044e97eb292fe444d2a24009b660294
Jon laptop <jon@laptop.com>
2025-03-10 16:39:05 -0600

    agregamos el campo oferta en los resultados de busqueda y modificamos el query para obtener el dato

--- admin/src/search-product.php
+++ admin/src/search-product.php
@@ -6,13 +6,13 @@ if (isset($_SESSION['ad-name']) && $_SESSION["rol"] === 0) {
 
     if (!empty($_POST['dato'])) {
         $buscar = $_POST['dato'];
-        $query = "SELECT p.ID, p.NOMBRE, l.NOMBRE AS LABORATORIO 
+        $query = "SELECT p.ID, p.NOMBRE, p.OFERTA, l.NOMBRE AS LABORATORIO 
         FROM product p 
         LEFT JOIN lab l ON p.ID_LAB = l.ID 
         WHERE p.NOMBRE LIKE CONCAT('%', :dato, '%') 
         LIMIT 20";
     } else {
-        $query = "SELECT p.ID, p.NOMBRE, l.NOMBRE AS LABORATORIO FROM product p LEFT JOIN lab l ON p.ID_LAB = l.ID ORDER BY p.ID DESC LIMIT 20";
+        $query = "SELECT p.ID, p.NOMBRE, p.OFERTA, l.NOMBRE AS LABORATORIO FROM product p LEFT JOIN lab l ON p.ID_LAB = l.ID ORDER BY p.ID DESC LIMIT 20";
     }
 
     $stmt = $con->prepare($query);
@@ -29,6 +29,7 @@ if (isset($_SESSION['ad-name']) && $_SESSION["rol"] === 0) {
             <tr>
                 <th><?php echo $data['NOMBRE']; ?></th>
                 <td><?php echo $data['LABORATORIO']; ?></td>
+                <td><?php echo $data['OFERTA']; ?></td>
                 <td>
                     <button type="button" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#edit"
                         data-bs-nombre="<?php echo $data['NOMBRE']; ?>" data-bs-id="<?php echo $data['ID']; ?>">
@@ -40,7 +41,7 @@ if (isset($_SESSION['ad-name']) && $_SESSION["rol"] === 0) {
         }
     } else {
         echo '<tr>
-        <td colspan="3">
+        <td colspan="4">
         <div class="alert alert-danger" role="alert">No se encontraron productos</div>
         </td>
         </tr>';
